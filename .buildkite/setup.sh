74747a8b0c3bea7725b80f33c1db4eafafdb0c29
gunjanrtc <156164197+gunjanrtc@users.noreply.github.com>
2024-04-04 15:30:10 +0530

    Update setup.sh

--- .buildkite/setup.sh
+++ .buildkite/setup.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+set -eo pipefail
+
 export SLAVE_PIPELINE_ORG="rtctek"
+echo $SLAVE_PIPELINE_ORG
 export SLAVE_PIPELINE="gunjan-test-pipeline"
+echo $SLAVE_PIPELINE
 export BUILDKITE_API_TOKEN="bkua_5b60deaf82f5775af82478873bab212805995acc"
+echo $BUILDKITE_API_TOKEN
 export BUILDKITE_API_URL="https://api.buildkite.com/v2"
+echo $BUILDKITE_API_URL
