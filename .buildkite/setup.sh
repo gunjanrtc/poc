c9d7e92f9daa6f095c4a3975e583b54bd342c5f3
gunjanrtc <156164197+gunjanrtc@users.noreply.github.com>
2024-04-11 14:43:40 +0530

    Update setup.sh

--- .buildkite/setup.sh
+++ .buildkite/setup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -eo pipefail
 
-export SLAVE_PIPELINE_ORG="rtctek"
-echo $SLAVE_PIPELINE_ORG
-export SLAVE_PIPELINE="gunjan-test-pipeline"
-echo $SLAVE_PIPELINE
-export BUILDKITE_API_TOKEN="bkua_5b60deaf82f5775af82478873bab212805995acc"
-echo $BUILDKITE_API_TOKEN
-export BUILDKITE_API_URL="https://api.buildkite.com/v2"
-echo $BUILDKITE_API_URL
+run(){
+  pwd
+  curl -H "Authorization: Bearer bkua_5b60deaf82f5775af82478873bab212805995acc" -X GET "https://api.buildkite.com/v2/organizations/rtctek/pipelines/master-testing-pipeline/builds/$BUILDKITE_BUILD_NUMBER" > master.json 
+  slaveBuild= $(jq '.jobs[1].triggered_build.number' master.json)
+  echo $BUILDKITE_BUILD_NUMBER
+  echo $slaveBuild
+}
