c2f78ee7f38730aad83c5aa2b4efbdc4651c63e1
gunjanrtc <156164197+gunjanrtc@users.noreply.github.com>
2024-04-12 15:32:47 +0530

    Update setup.sh

--- .buildkite/setup.sh
+++ .buildkite/setup.sh
@@ -5,7 +5,9 @@ run(){
   echo "$BUILDKITE_BUILD_NUMBER"
   curl -s -H "Authorization: Bearer bkua_5b60deaf82f5775af82478873bab212805995acc" -X GET "https://api.buildkite.com/v2/organizations/rtctek/pipelines/slave-pipeline/builds" | jq '.[0].number'
 }
-
+echo "chl"
 if [[ "$1" == "run" ]]; then
     run
+else 
+  echo "don't run"
 fi
