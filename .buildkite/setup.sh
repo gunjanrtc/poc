df3c22ef6b016aa8248f0671df3f2c75b10dcec4
gunjanrtc <156164197+gunjanrtc@users.noreply.github.com>
2024-04-12 15:07:34 +0530

    Update setup.sh

--- .buildkite/setup.sh
+++ .buildkite/setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -eo pipefail
-
+run(){
   pwd
   curl -H "Authorization: Bearer bkua_5b60deaf82f5775af82478873bab212805995acc" -X GET "https://api.buildkite.com/v2/organizations/rtctek/pipelines/slave-pipeline/builds")
   LATEST_BUILD_NUMBER=$(echo $BUILDS | jq '.[0].number')
   echo "Latest build number: $LATEST_BUILD_NUMBER"
+  }
+  if [[ "$1" == "run" ]]; then
+    setup
+fi
