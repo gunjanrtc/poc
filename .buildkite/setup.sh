454d2aba6fbca7fea3a8b27927db909ac1bc3176
gunjanrtc <156164197+gunjanrtc@users.noreply.github.com>
2024-04-12 15:12:20 +0530

    Update setup.sh

--- .buildkite/setup.sh
+++ .buildkite/setup.sh
@@ -2,11 +2,7 @@
 set -eo pipefail
 
 run(){
-  pwd
-  curl -H "Authorization: Bearer bkua_5b60deaf82f5775af82478873bab212805995acc" -X GET "https://api.buildkite.com/v2/organizations/rtctek/pipelines/master-testing-pipeline/builds/$BUILDKITE_BUILD_NUMBER" > master.json 
-  slaveBuild= $(jq '.jobs[1].triggered_build.number' master.json)
-  echo $BUILDKITE_BUILD_NUMBER
-  echo $slaveBuild
+  echo $(curl -s -H "Authorization: Bearer bkua_5b60deaf82f5775af82478873bab212805995acc" -X GET "https://api.buildkite.com/v2/organizations/rtctek/pipelines/slave-pipeline/builds") | jq '.[0].number'
 }
 
 if [[ "$1" == "run" ]]; then
